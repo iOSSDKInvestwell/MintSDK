cef8265e93e57665f587ef1d45b17232f54b1739
Ground1 <dharmendra@investwellonline.com>
2026-09-18 15:47:03 +0530

    1.0.5: dynamic wrapper products (fix undefined symbols), revert ForceLink

--- Package.swift
+++ Package.swift
@@ -9,10 +9,12 @@ let package = Package(
     products: [
         .library(
             name: "MintSDK",
+            type: .dynamic,
             targets: ["MintFrameworksWrapper"]
         ),
         .library(
             name: "MintFrameworks",
+            type: .dynamic,
             targets: ["MintFrameworksWrapper"]
         )
     ],
